67542d966e2c4256a1eb78a7dd303e981a8f9407
mykolasbar <106963436+mykolasbar@users.noreply.github.com>
2022-12-14 01:38:34 +0200

    Added some calculations from props to sliding gallery (v2)

--- src/slidinggallery2.js
+++ src/slidinggallery2.js
@@ -3,11 +3,12 @@ import React, { useState, useEffect, useContext, useRef, useCallback }  from 're
 const Slidinggallery2 = (props) => {
     let [imgindex, setImgIndex] = useState(0)
     let [percentage, setPercentage] = useState(0)
+    let [width, setWidth] = useState(100)
 
     useEffect(() => {
         setTimeout(() => {
-            setImgIndex(imgindex+=1)
-            setPercentage(percentage += 25)
+            setImgIndex(imgindex += 1)
+            setPercentage(percentage += 100/props.images.length)
             if (imgindex == props.images.length)
                 {setImgIndex(imgindex = 0)
                 setPercentage(percentage = 0)
@@ -15,19 +16,18 @@ const Slidinggallery2 = (props) => {
         }, 3000);
     }, [imgindex]);
     
+    useEffect(()=>{
+        setWidth(width = width * props.images.length)
+        console.log(width)
+    }, []);
 
 
     return (
         <div style={{overflow:"hidden", width: "75%", margin: "15px auto"}}>
-            <div style={{width:"400%", display:"inline-flex", flexDirection:"row", flexWrap:"nowrap", whiteSpace: "no-wrap", transition:"transform 0.3s", transform: "translateX(-" + percentage + "%)"}}>
+            <div style={{width: width + "%", display:"inline-flex", flexDirection:"row", flexWrap:"nowrap", whiteSpace: "no-wrap", transition:"transform 0.3s", transform: "translateX(-" + percentage + "%)"}}>
                 {props.images.map((image) => <div style = {{backgroundImage: "URL(" + image.src + ")", backgroundSize: "cover", backgroundPosition: "center", display:"inline-flex", height: "220px", width:"100%"}}></div>)}
             </div>
         </div>
-
-        // <div className = "container-fluid w-75 p-3 d-flex align-items-center" style={{height: "300px"}}>
-        //     <div className = "container-fluid w-100 h-100" style = {{backgroundImage: "URL(" + props.images[imgindex].src + ")", backgroundSize: "cover", backgroundPosition: "center"}}>
-        //     </div>
-        // </div>
     );
 };
 
