dd79d4f0449326933671e1d61ea9bb89ae5480ac
mykolasbar <106963436+mykolasbar@users.noreply.github.com>
2023-01-12 01:48:14 +0200

    Fixing deployement issues

--- src/ShowSchool.js
+++ src/ShowSchool.js
@@ -6,9 +6,11 @@ const ShowSchool = ({title, picture, children}) => {
 
     return (
         <div id = "schoolBox" onMouseEnter={() => {setIsActive(true)}} onMouseLeave={(event) => {setIsActive(false)}}><div style = {{backgroundColor: "black", color: "white", borderRadius: "10px 10px 0 0", padding: "5px", width: "100%" }}>{title}</div> 
-            { !isActive ? <div style = {{ padding: "5px", backgroundSize: "cover", backgroundPosition: "center", backgroundImage: "URL(https://ugdymoistaigosfe.herokuapp.com/" + picture + ")", backgroundColor: "#CCE5FF", height: "150px", fontSize: "0", borderRadius: "0 0 10px 10px"}}>aa</div> : children }
+            { !isActive ? <div style = {{ padding: "5px", backgroundSize: "cover", backgroundPosition: "center", backgroundImage: "URL(https://ugdymoistaigosbe.herokuapp.com/images/mokykla3.jpg)", backgroundColor: "#CCE5FF", height: "150px", fontSize: "0", borderRadius: "0 0 10px 10px"}}>aa</div> : children }
         </div>
     );
 };
 
 export default ShowSchool;
+
+// " + picture + "
