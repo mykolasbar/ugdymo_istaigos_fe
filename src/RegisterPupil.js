5595a3060c18621922b3c174e2e7f86522115587
mykolasbar <106963436+mykolasbar@users.noreply.github.com>
2023-01-17 16:23:24 +0200

    Style fixes

--- src/RegisterPupil.js
+++ src/RegisterPupil.js
@@ -29,9 +29,10 @@ const RegisterPupil = (props) => {
 
     useEffect(() => {
         let id = auth.getUser()?.id;
+        console.log(userId)
         fetch("https://ugdymoistaigosbe.herokuapp.com/api/pupils/" + userId, {method: 'GET', headers: { 'Content-Type': 'application/json' }})
         .then(response => response.json())
-        .then((result) => {setPupils(pupils = result)})
+        .then((result) => {return setPupils(pupils = result)})
         }, [userId]);
 
     useEffect(() => {
@@ -70,13 +71,13 @@ const RegisterPupil = (props) => {
         
     return (
         <>
-        {auth.isLoggedin() || auth.isLoggedinAdmin() &&  
-            <form id = "pupilwindow" className = "container p-4 mt-5">
-                {showNotif && status}<br/>
-                {pupils.length === 0 ? "" :
+        {(auth.isLoggedin() || auth.isLoggedinAdmin()) &&  
+            <form id = "pupilwindow" className = "container p-4 mt-2">
+                {showNotif && <text className = "mb-4">{status}<br/></text>}
+                {pupils.length == 0 ? <div className = "mb-3">Kol kas nesate pridėję jokių mokinių. Kad galėtumėte užregistruoti mokinį į šią mokyklą, pridėkite užpildykite mokinio duomenis:</div> :
                 <div>
                     <label><b>Pasirinkite mokinį</b></label> 
-                    <select name = "requests_id" className="form-control  mb-2 mt-2" onChange={(event)=>{setPupilId(pupilId = event.target.value); console.log(pupilId)}}>
+                    <select name = "requests_id" className="form-control mb-2 mt-2" onChange={(event)=>{setPupilId(pupilId = event.target.value); console.log(pupilId)}}>
                         <option value="N/A" className="form-control" ></option>
                         { pupils.map((pupil) => 
                         <option name = "requests_id" className = "m-4" key = {pupil.id} value = {pupil.id}> 
@@ -84,8 +85,8 @@ const RegisterPupil = (props) => {
                         </option>) 
                         }
                     </select> 
-                </div>}
                 arba
+                </div>}
                 <span style = {{display: showNotif ? "block" : "none", position: "absolute"}}>{status}</span>
                 <span className = "card border-0 mt-1"><b>Pridėkite naują mokinį</b></span>
                 <div className="form-group mt-2">
@@ -102,13 +103,13 @@ const RegisterPupil = (props) => {
                 </div>
                     <input type = "submit" value = "Pridėti" className="btn btn-dark btn-sm m-2" onClick = {handleSubmit}></input>
                     <input type = "submit" value = "Atšaukti" className="btn btn-danger btn-sm m-2" onClick={()=>{props.closeModal(); props.enableScroll()}}></input>
-                </form>}
-                {!auth.isLoggedin() && !auth.isLoggedinAdmin() && 
-                <div style={{zIndex:"2000", width:"40vw", position:"fixed", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", borderRadius: "10px", position: "fixed",  backgroundColor: "white", left:"30%", top:"15%"}} className = "container mt-5">
-                    <div style={{display:"flex", flexDirection:"row", width:"100%", justifyContent:"flex-end", padding:"0 0 0 0"}}><span className="material-symbols-outlined" style={{cursor:"pointer"}} onClick={()=>{props.closeModal(); props.enableScroll()}}>close</span></div>
-                    <div style = {{ backgroundSize: "cover", backgroundImage: "URL(http://localhost:8000/" + props.picture + ")", backgroundColor: "#CCE5FF", height: "150px", width:"100%", fontSize: "0", borderRadius: "10px 10px 0 0"}}>aa</div>
-                    <div style={{padding:"10px"}}>Kad galėtumėte užregistruoti mokinį į mokyklą, turite <Link to="/login">prisijungti</Link>.</div>
-                </div>}
+            </form>}
+        {!auth.isLoggedin() && !auth.isLoggedinAdmin() && 
+            <div style={{zIndex:"2000", width:"40vw", position:"fixed", boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)", borderRadius: "10px", position: "fixed",  backgroundColor: "white", left:"30%", top:"15%"}} className = "container mt-5">
+                <div style={{display:"flex", flexDirection:"row", width:"100%", justifyContent:"flex-end", padding:"0 0 0 0", position:"absolute", left:"-20px", top:"15px"}}><span id="closeIcon" className="material-symbols-outlined" style={{cursor:"pointer"}} onClick={()=>{props.closeModal(); props.enableScroll()}}>close</span></div>
+                <div style = {{ backgroundSize: "cover", backgroundPosition: "center", backgroundImage: "URL(https://mybucket1-ugdymoistaigos.s3.eu-west-1.amazonaws.com/" + props.picture + ")", backgroundColor: "#CCE5FF", height: "200px", width:"100%", fontSize: "0", borderRadius: "10px 10px 0 0", marginTop:"13px"}}>aa</div>
+                <div style={{padding:"15px"}}>Kad galėtumėte užregistruoti mokinį į mokyklą, turite <Link to="/login">prisijungti</Link>.</div>
+            </div>}
         </>
     );
 };
