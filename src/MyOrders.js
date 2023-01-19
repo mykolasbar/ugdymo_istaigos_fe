1ddf71876feee6f2861f530dc2fde335c53a8bad
mykolasbar <106963436+mykolasbar@users.noreply.github.com>
2023-01-19 18:12:31 +0200

    Updating user account style

--- src/MyOrders.js
+++ src/MyOrders.js
@@ -27,12 +27,11 @@ const MyOrders = () => {
     return (
         <>
             {loading ? <div style = {{height:"50vw", width: "100%", display:"flex", justifyContent:"center", alignItems:"center"}}>Turinys kraunasi <span className="material-symbols-outlined">hourglass_top</span></div> :
-            (orders.length > 0 ? 
-            <div style={{width:"100%", display:"inline-grid", flexWrap:"wrap", gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))", gridAutoRows:"240px", rowGap:"20px"}}>
+            (orders.length > 0 ?
+            <div style={{width:"100%", display:"inline-grid", flexWrap:"wrap", gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))", gridAutoRows:"200px", rowGap:"20px", justifyItems:"center"}}>
                 {orders.map((order, index) =>
                     <div key = {order.id} style = {{margin: "8px", backgroundSize: "cover", backgroundPosition: "center", maxWidth:"300px"}}>
-                        <div style = {{backgroundColor: "#CCE5FF", color: "white", borderRadius: "10px 10px 0 0", padding: "8px", width: "100%"}}>{order.name}</div>
-                        <div style ={{padding:"8px", backgroundColor: "#CCE5FF", borderRadius: "0 0 10px 10px", minHeight:"200px", }}>
+                        <div style ={{padding:"8px", backgroundColor: "#ebfaf9", borderRadius: "10px 10px 10px 10px", minHeight:"200px", }}>
                             <b>Užsakymo ID: </b> {order.id} <br/> <b>Mokinio vardas, pavardė: </b>{order.requests.name} <br/> <b>Mokinio asmens kodas: </b>{order.requests.idnumber} <br/> <b>Klasė: </b>{order.requests.class} <br /><b>Užsakymo statusas:</b> {order.confirmed ? "Patvirtinta" : "Laukiama patvirtinimo"}
                         </div>
                     </div>)}
