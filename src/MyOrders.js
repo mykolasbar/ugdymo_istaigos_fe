cde3dee25f451732102b4604d91282a155c60858
mykolasbar <106963436+mykolasbar@users.noreply.github.com>
2023-01-19 16:24:20 +0200

    Style updates for account section

--- src/MyOrders.js
+++ src/MyOrders.js
@@ -28,15 +28,16 @@ const MyOrders = () => {
         <>
             {loading ? <div style = {{height:"50vw", width: "100%", display:"flex", justifyContent:"center", alignItems:"center"}}>Turinys kraunasi <span className="material-symbols-outlined">hourglass_top</span></div> :
             (orders.length > 0 ? 
-            <table className = "p-4 w-100">
-                <thead style={{fontWeight:"bold", backgroundColor:"#e38e84"}}><tr><td style={{padding:"12px"}}>Užsakymo ID</td><td>Mokinio vardas, pavardė</td><td>Mokinio asmens kodas</td><td>Mokinio klasė</td><td>Užsakymo statusas</td></tr></thead>
-                <tbody>
-                { orders.map((order) => 
-                <tr id = "adminrow" className = "m-4" key = {order.id}> 
-                    <td style={{padding:"24px"}}>{order.id}</td><td>{order.requests.name}</td><td>{order.requests.idnumber}</td><td>{order.requests.class}</td><td>{order.confirmed ? "Patvirtinta" : "Laukiama patvirtinimo"}</td>
-                </tr>) 
-                }<tr></tr></tbody>
-            </table> : 
+            <div style={{width:"100%", display:"inline-grid", flexWrap:"wrap", gridTemplateColumns: "repeat(auto-fill, minmax(250px, 1fr))", gridAutoRows:"240px", rowGap:"20px"}}>
+                {orders.map((order, index) =>
+                    <div key = {order.id} style = {{margin: "8px", backgroundSize: "cover", backgroundPosition: "center", maxWidth:"300px"}}>
+                        <div style = {{backgroundColor: "#CCE5FF", color: "white", borderRadius: "10px 10px 0 0", padding: "8px", width: "100%"}}>{order.name}</div>
+                        <div style ={{padding:"8px", backgroundColor: "#CCE5FF", borderRadius: "0 0 10px 10px", minHeight:"200px", }}>
+                            <b>Užsakymo ID: </b> {order.id} <br/> <b>Mokinio vardas, pavardė: </b>{order.requests.name} <br/> <b>Mokinio asmens kodas: </b>{order.requests.idnumber} <br/> <b>Klasė: </b>{order.requests.class} <br /><b>Užsakymo statusas:</b> {order.confirmed ? "Patvirtinta" : "Laukiama patvirtinimo"}
+                        </div>
+                    </div>)}
+            </div>
+            : 
             <div style={{padding:"10px"}}>Kol kas nepadarėte jokių užsakymų.</div>)}
         </>
     );
--- src/MyPupils.js
+++ src/MyPupils.js
@@ -53,23 +53,17 @@ const MyPupils = () => {
             {(showDeleteModal === true || showEditModal === true) && <div style={{width:"100vw", height:"100vh", position:"fixed", backgroundColor:"grey", top: "0%", opacity:"0.6", zIndex:"1000"}}></div>}
             {loading ? (<div style = {{height:"50vw", width: "100%", display:"flex", justifyContent:"center", alignItems:"center"}}>Turinys kraunasi <span className="material-symbols-outlined">hourglass_top</span></div>) :
                 (orders.length > 0 ? 
-                <table className = "p-4 w-100" style={{zIndex:"900"}} id = "itemstable">
-                    <thead style={{fontWeight:"bold", backgroundColor:"#42e3f5"}}><tr><td style={{padding:"12px"}}>Mokinio ID sistemoje</td><td>Mokinio vardas, pavardė</td><td>Mokinio asmens kodas</td><td>Mokinio gimimo data</td><td>Mokinio klasė</td><td></td><td></td></tr></thead>
-                    <tbody>
-                        {orders.map((order) => 
-                        <tr id = "adminrow" className = "m-4" key = {order.id}> 
-                            <td style={{padding:"12px"}}>{order.id}</td> 
-                            <td>{order.name}</td>
-                            <td>{order.idnumber}</td>
-                            <td>{order.dateofbirth}</td>
-                            <td>{order.class}</td> 
-                            <td><input className="btn btn-danger btn-sm m-3 rounded-0" type = "button" onClick = {() => {setDeletionId(deletionId=order.id); setShowDeleteModal(true); disableScroll()}} value = "Trinti mokinį"></input></td>
-                            <td><button className="btn btn-dark btn-sm m-3 rounded-0" onClick={()=>{setPupilData(pupilData=order); setShowEditModal(true); disableScroll()}}>Atnaujinti mokinio duomenis</button></td>
-                        </tr>
-                        )
-                        }<tr></tr>
-                    </tbody>
-                </table> : 
+                    orders.map((order, index) =>
+                        <div key = {order.id} style = {{margin: "8px", backgroundSize: "cover", backgroundPosition: "center", minWidth:"200px", maxWidth:"300px"}}>
+                            <div style = {{backgroundColor: "black", color: "white", borderRadius: "10px 10px 0 0", padding: "8px", width: "100%"}}>{order.name}</div>
+                            <div style ={{padding:"8px", backgroundColor: "#CCE5FF", borderRadius: "0 0 10px 10px", minHeight:"240px", }}>
+                                <div style={{minHeight:"120px"}}><b>Mokinio vardas, pavardė: </b> {order.name} <br/> <b>Mokyklos adresas: </b>{order.idnumber} <br/> <b>Gimimo data: </b>{order.dateofbirth} <br/> <b>Klasė: </b>{order.class} </div> 
+                                <input className="btn btn-danger btn-sm m-2 rounded-0" type = "button" onClick = {() => {setDeletionId(deletionId=order.id); setShowDeleteModal(true); disableScroll()}} value = "Trinti mokinį"></input>
+                                <button className="btn btn-dark btn-sm m-2 rounded-0" onClick={()=>{setPupilData(pupilData=order); setShowEditModal(true); disableScroll()}}>Atnaujinti mokinio duomenis</button>
+                            </div>
+                        </div>
+                    )
+                : 
                 <div style={{padding:"10px"}}>Pridėtų mokinių nėra.</div>)}
                 {showEditModal && <EditPupil pupilData={pupilData} closeModal = {closeModal} refreshing = {refreshing} enableScroll = {enableScroll}/>}
                 {showDeleteModal && <DeletePrompt deletionId={deletionId} closeModal = {closeModal} refreshing = {refreshing} enableScroll = {enableScroll}/>}
